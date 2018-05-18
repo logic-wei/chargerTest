142cbf76dba4b7be0cfb471c7392619044120b44
weipeng <weipeng@meizu.com>
2018-05-18 13:26:12 +0800

    optimize code style

--- mpower_tools.py
+++ mpower_tools.py
@@ -8,7 +8,7 @@ import copy
 
 from dateutil import parser
 
-keyWordsMap = {
+KEY_WORDS_PATTERN = {
     "time":     "^\[(.*?)\].*",
     "flag":     ".*\[mpower\].*",
     "chg":      ".*\[charger\].*",
@@ -34,110 +34,122 @@ keyWordsMap = {
     "dischg":   ".*<is_disable_charge=(\d)>.*"
 }
 
-class ChargerState:
-    "represent charge state at one moment"
-    time = parser.parse("1900-1-1 00:00:00")
-    chgtyp = "unknown"
-    cc = 0
-    icl = 0
-    brdtmp = 0
-    petyp = "unknown"
-    vbus = 0
-    ulmt = 0
-    scrn = 0
-    fstchg = 0
-    usoc = 0
-    tsoc = 0
-    battmp = 0
-    vbat = 0
-    ibat = 0
-    usbtmp = 0
-    usbvltg = 0
-    dischg = 0
-    plg = 0
+KEY_WORDS_VALUE = {
+    "time":     parser.parse("1900-1-1 00:00:00"),
+    "chgtyp":   "unknown",
+    "cc":       0,
+    "icl":      0,
+    "brdtmp":   0.0,
+    "petyp":    "unknown",
+    "vbus":     0,
+    "ulmt":     0,
+    "scrn":     0,
+    "fstchg":   0,
+    "usoc":     0,
+    "tsoc":     0,
+    "battmp":   0.0,
+    "vbat":     0,
+    "ibat":     0,
+    "usbtmp":   0.0,
+    "usbvltg":  0,
+    "dischg":   0,
+    "plg":      0
+}
+
+INT_KEY_WORDS = (
+    "cc", "icl", "brdtmp", "vbus", "ulmt",
+    "scrn", "fstchg", "usoc", "tsoc", "vbat",
+    "ibat", "usbvltg", "dischg"
+)
+
+FLT_KEY_WORDS = (
+    "brdtmp", "battmp", "usbtmp"
+)
+
+STR_KEY_WORDS = (
+    "chgtyp", "petyp"
+)
+
+SPC_KEY_WORDS = (
+    'plg', "time"
+)
+
 
 def parse(path):
-    global keyWordsMap
-    chgStatCur = ChargerState()
-    chgStatPre = [chgStatCur]
+    global KEY_WORDS_PATTERN
+    global KEY_WORDS_VALUE
+
+    key_words_val_cur = copy.deepcopy(KEY_WORDS_VALUE)
+    key_words_val_ser = []
+
+    pattern_compiled = {}
+    for key_word in KEY_WORDS_PATTERN:
+        pattern_compiled[key_word] = re.compile(KEY_WORDS_PATTERN[key_word])
+
     with open(path, "r") as log:
         for line in log:
             # valid log
-            if re.match(keyWordsMap["flag"], line):
+            if pattern_compiled["flag"].match(line):
                 # parse the tag type
-                tagType = "unknown"
-                if re.match(keyWordsMap["chg"], line):
-                   tagType = "chg"
-                elif re.match(keyWordsMap["fg"], line):
-                    tagType = "fg"
-                elif re.match(keyWordsMap["usb"], line):
-                    tagType = "usb"
-                # parse the tag
-                for keyWord in keyWordsMap:
-                    # print(keyWord)
-                    match = re.match(keyWordsMap[keyWord], line)
+                tag_type = "unknown"
+                if pattern_compiled["chg"].match(line):
+                    tag_type = "chg"
+                elif pattern_compiled["fg"].match(line):
+                    tag_type = "fg"
+                elif pattern_compiled["usb"].match(line):
+                    tag_type = "usb"
+                else:
+                    continue
+
+                # parse all the tag
+                for key_word in INT_KEY_WORDS:
+                    match = pattern_compiled[key_word].match(line)
                     if match:
-                        # for debug
-                        # if keyWord != "flag" and keyWord != "fg" and keyWord != "chg" and keyWord != "usb":
-                        #     print(match.group(1))
-                        # parse all key words
-                        if keyWord == "time":
-                            chgStatCur.time = parser.parse(match.group(1))
-                        elif keyWord == "plg":
+                        key_words_val_cur[key_word] = int(match.group(1))
+                for key_word in FLT_KEY_WORDS:
+                    match = pattern_compiled[key_word].match(line)
+                    if match:
+                        key_words_val_cur[key_word] = float(match.group(1))
+                for key_word in STR_KEY_WORDS:
+                    match = pattern_compiled[key_word].match(line)
+                    if match:
+                        key_words_val_cur[key_word] = match.group(1)
+                for key_word in SPC_KEY_WORDS:
+                    match = pattern_compiled[key_word].match(line)
+                    if match:
+                        if key_word == "plg":
                             if match.group(1) == "in":
-                                chgStatCur.plg = 1
+                                key_words_val_cur[key_word] = 1
                             elif match.group(1) == "out":
-                                chgStatCur.plg = 0
-                        elif keyWord == "chgtyp":
-                            chgStatCur.chgtyp = match.group(1)
-                        elif keyWord == "cc":
-                            chgStatCur.cc = int(match.group(1))
-                        elif keyWord == "icl":
-                            chgStatCur.icl = int(match.group(1))
-                        elif keyWord == "brdtmp":
-                            chgStatCur.brdtmp = float(match.group(1))
-                        elif keyWord == "petyp":
-                            chgStatCur.petyp = match.group(1)
-                        elif keyWord == "vbus":
-                            chgStatCur.vbus = int(match.group(1))
-                        elif keyWord == "ulmt":
-                            chgStatCur.ulmt = int(match.group(1))
-                        elif keyWord == "scrn":
-                            chgStatCur.scrn = int(match.group(1))
-                        elif keyWord == "fstchg":
-                            chgStatCur.fstchg = int(match.group(1))
-                        elif keyWord == "usoc":
-                            chgStatCur.usoc = int(match.group(1))
-                        elif keyWord == "tsoc":
-                            chgStatCur.tsoc = int(match.group(1))
-                        elif keyWord == "battmp":
-                            chgStatCur.battmp = float(match.group(1))
-                        elif keyWord == "vbat":
-                            chgStatCur.vbat = int(match.group(1))
-                        elif keyWord == "ibat":
-                            chgStatCur.ibat = int(match.group(1))
-                        elif keyWord == "usbtmp":
-                            chgStatCur.usbtmp = float(match.group(1))
-                        elif keyWord == "usbvltg":
-                            chgStatCur.usbvltg = int(match.group(1))
-                        elif keyWord == "dischg":
-                            chgStatCur.dischg = int(match.group(1))
-                chgStatPre.append(chgStatCur)
-                chgStatCur = copy.copy(chgStatPre[-1])
-        return chgStatPre
+                                key_words_val_cur[key_word] = 0
+                        elif key_word == "time":
+                            key_words_val_cur["time"] = parser.parse(match.group(1))
+
+                key_words_val_ser.append(key_words_val_cur)
+                key_words_val_cur = copy.deepcopy(key_words_val_ser[-1])
+        return key_words_val_ser
 
 # under dev
-def plotChgStatSeries(chgStatSeries):
+def plot_key_words_value(values):
     y = []
-    for chgStat in chgStatSeries:
-        y.append(chgStat.vbat)
+    for value in values:
+        y.append(value["time"])
     x = range(len(y))
     plt.plot(x, y)
     plt.show()
 
+def plot_key_words_value2(values):
+    y = []
+    x = []
+    for value in values:
+        x.append(value["time"])
+        y.append(value["vbat"])
+    plt.plot(x, y)
+    plt.show()
 
+#under dev
 def main():
-    logPath = ""
+    log_path = ""
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hl:v", ["help", "log", "version"])
     except getopt.GetoptError:
@@ -149,9 +161,9 @@ def main():
         elif opt in ("-v", "--version"):
             print("version: v0.1")
         elif opt in ("-l", "--log"):
-            logPath = arg
-    chgStatSeries = parse(logPath)
-    plotChgStatSeries(chgStatSeries)
+            log_path = arg
+    values = parse(log_path)
+    plot_key_words_value2(values)
 
 
 if __name__ == "__main__":
